79b08edc3d3898708ca8bdd3606ccb0aff8b1519
0xKate <94711905+0xKate@users.noreply.github.com>
2024-06-09 18:21:51 -0700

    Add Saving and Loading

--- Controller/App.xaml.cs
+++ Controller/App.xaml.cs
@@ -1,21 +1,59 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Text.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace EVEAutoInvite
 {
     public partial class App : Application
     {
+        public ObservableCollection<ESIAuthenticatedCharacter> AuthenticatedCharacters { get; private set; }
+        public readonly string AuthBackupFile = "Saved-Characters.json";
 
-        protected override void OnStartup(StartupEventArgs e)
+        public async Task<bool> LoadCharactersAsync()
+        {
+            if (File.Exists(this.AuthBackupFile))
+            {
+                using (StreamReader reader = new StreamReader(this.AuthBackupFile))
+                {
+                    string json = await reader.ReadToEndAsync();
+                    if (json.Length > 2)
+                    {
+                        this.AuthenticatedCharacters = JsonSerializer.Deserialize<ObservableCollection<ESIAuthenticatedCharacter>>(json);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public async Task SaveCharactersAsync()
+        {
+            string json = JsonSerializer.Serialize(this.AuthenticatedCharacters);
+            using (StreamWriter writer = new StreamWriter(this.AuthBackupFile))
+            {
+                await writer.WriteAsync(json);
+            }
+        }
+
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            // Load the Authenticated Characters from file
+            await this.LoadCharactersAsync();            
 
             // Create and show the main window
             MainWindow = new MainWindow();
             MainWindow.Show();
         }
 
-        protected override void OnExit(ExitEventArgs e)
+        protected override async void OnExit(ExitEventArgs e)
         {
+            await this.SaveCharactersAsync();
+
             base.OnExit(e);
         }
     }
